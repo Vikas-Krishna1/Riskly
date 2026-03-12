f3a0b7d6672290b50a496a01accb43f11f1e0ac5
Vikas-Krishna1 <150981669+Vikas-Krishna1@users.noreply.github.com>
2026-03-12 18:06:34 -0400

    API added to Frontend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default configuration
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: "https://riskly.onrender.com/",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
